7b7d09e90ff7858fe53619d1fcacd73b6f59e58d
larytet <larytet@yahoo.com>
2017-09-18 13:59:28 +0300

    Frontend based on Flask

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #iptables -I INPUT -p tcp --dport 5000 -j ACCEPT
 current_dir=`pwd`
 cd `dirname $0`
-init_path=`pwd`/yalas/__init__.py
+init_path=`pwd`/yalas/app.py
 echo Running with $init_path
 FLASK_DEBUG=1  FLASK_APP=$init_path flask run --host=0.0.0.0
 cd current_dir
