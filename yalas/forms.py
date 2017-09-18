51dac4374445987c17428aad3a6226c8f88d56b1
larytet <larytet@yahoo.com>
2017-09-18 19:04:43 +0300

    Frontend based on Flask

--- yalas/forms.py
+++ yalas/forms.py
@@ -3,6 +3,6 @@ import wtforms
 
 class SearchForm(flask_wtf.Form):
     # validators=[wtforms.validators.DataRequired(), wtforms.validators.Email()]
-    search_form = wtforms.TextField('Search')
+    search_form = wtforms.TextField('Search', validators=None)
     
     
