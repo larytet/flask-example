a424ac8f4add0ee6f6cb8ccfb217b5cf4dbb9780
larytet <larytet@yahoo.com>
2017-09-20 18:54:57 +0300

    System test

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #iptables -I INPUT -p tcp --dport 5000 -j ACCEPT
 current_dir=`pwd`
 cd `dirname $0`
-init_path=`pwd`/yalas/app.py
+init_path=`pwd`/yalas
 echo Running with $init_path
-FLASK_DEBUG=1  FLASK_APP=$init_path flask run --host=0.0.0.0
+FLASK_DEBUG=1  FLASK_APP=$init_path/app.py flask run --host=0.0.0.0 &
+python $init_path/app_test.py
 cd current_dir
--- yalas/app_test.py
+++ yalas/app_test.py
@@ -11,8 +11,6 @@ class AppTestCase(unittest.TestCase):
         self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
         app.testing = True
         self.app = app.test_client()
-        with app.app_context():
-            app.init_db()
 
     def tearDown(self):
         os.close(self.db_fd)
