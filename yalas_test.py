599f93f5e6ebeca2f9568fddf54150a257e9c398
larytet <larytet@yahoo.com>
2017-09-20 21:11:50 +0300

    System test

--- yalas_test.py
+++ yalas_test.py
@@ -8,13 +8,11 @@ from yalas import app
 class AppTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
         app.testing = True
         self.app = app.test_client()
 
     def tearDown(self):
         os.close(self.db_fd)
-        os.unlink(app.config['DATABASE'])
 
     def test_index(self):
         rv = self.app.get('/')
