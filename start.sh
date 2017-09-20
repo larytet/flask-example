bb1bc3aded3ee76ea03193dad89bf2cbcb2b7112
larytet <larytet@yahoo.com>
2017-09-20 19:01:38 +0300

    System test

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@ current_dir=`pwd`
 cd `dirname $0`
 init_path=`pwd`/yalas
 echo Running with $init_path
-FLASK_DEBUG=1  FLASK_APP=$init_path/app.py flask run --host=0.0.0.0 
-#python $init_path/app_test.py
+FLASK_DEBUG=1  FLASK_APP=$init_path/app.py flask run --host=0.0.0.0 &
+flask_process_id = $!
+python $init_path/../yalas_test.py
+kill $flask_process_id
 cd $current_dir
