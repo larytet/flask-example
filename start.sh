b180f3472100044e0be6ba9800bbace9e4a327d1
larytet <larytet@yahoo.com>
2017-09-20 18:57:26 +0300

    System test

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ cd `dirname $0`
 init_path=`pwd`/yalas
 echo Running with $init_path
 FLASK_DEBUG=1  FLASK_APP=$init_path/app.py flask run --host=0.0.0.0 &
-python $init_path/app_test.py
+#python $init_path/app_test.py
 cd $current_dir
